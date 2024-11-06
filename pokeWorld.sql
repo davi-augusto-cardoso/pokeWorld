3da90ea33c3708c0bbe382e770fca15ba5cc9149
Davi Cardoso <daviaugustocardoso44@gmail.com>
2024-11-06 11:55:41 -0300

    Código do SQl para gerar o banco

---
+++ pokeWorld.sql
@@ -0,0 +1,131 @@
+DROP DATABASE IF EXISTS pokemon;
+CREATE DATABASE pokemon;
+USE pokemon;
+
+/* Lógico_3: */
+
+CREATE TABLE Regiao (
+    nome VARCHAR(20),
+    ID_regial INT PRIMARY KEY
+);
+
+CREATE TABLE Pokemon (
+    nome VARCHAR(20),
+    Id_pokemon INT PRIMARY KEY,
+    forca INT,
+    resistencia INT,
+    velocidade INT,
+    peso FLOAT,
+    Shyne BOOLEAN,
+    nivel INT,
+    fk_Party_id_Party INT,
+    selvagem BOOLEAN DEFAULT true
+);
+
+CREATE TABLE Treinador (
+    Nome VARCHAR(50),
+    data_nasc DATE,
+    genero VARCHAR(20),
+    CPF VARCHAR(20),
+    ID_treinador INT PRIMARY KEY
+);
+
+CREATE TABLE movimentos (
+    Id_Movimento INT PRIMARY KEY,
+    dano INT,
+    chanceDeAcerto FLOAT
+);
+
+CREATE TABLE Party (
+    id_Party INT PRIMARY KEY AUTO_INCREMENT,
+    fk_Treinador_ID_treinador INT
+);
+
+CREATE TABLE Elemento (
+    id_elemento INT PRIMARY KEY,
+    Nome VARCHAR(20)
+);
+
+CREATE TABLE poke_regiao (
+    fk_Regiao_ID_regial INT NOT NULL,
+    fk_Pokemon_Id_pokemon INT NOT NULL,
+    PRIMARY KEY (fk_Regiao_ID_regial, fk_Pokemon_Id_pokemon)
+);
+
+CREATE TABLE poke_mov (
+    fk_Pokemon_Id_pokemon INT NOT NULL,
+    fk_movimentos_Id_Movimento INT NOT NULL,
+    PRIMARY KEY (fk_movimentos_Id_Movimento, fk_Pokemon_Id_pokemon)
+);
+
+CREATE TABLE Poke_elemento (
+    fk_Pokemon_Id_pokemon INT NOT NULL,
+    fk_Elemento_id_elemento INT NOT NULL,
+    PRIMARY KEY (fk_Pokemon_Id_pokemon, fk_Elemento_id_elemento)
+);
+ 
+ALTER TABLE Pokemon ADD CONSTRAINT FK_Pokemon_2
+    FOREIGN KEY (fk_Party_id_Party)
+    REFERENCES Party (id_Party)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE Party ADD CONSTRAINT FK_Party_2
+    FOREIGN KEY (fk_Treinador_ID_treinador)
+    REFERENCES Treinador (ID_treinador)
+    ON DELETE CASCADE;
+ 
+ALTER TABLE poke_regiao ADD CONSTRAINT FK_poke_regiao_1
+    FOREIGN KEY (fk_Regiao_ID_regial)
+    REFERENCES Regiao (ID_regial)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE poke_regiao ADD CONSTRAINT FK_poke_regiao_2
+    FOREIGN KEY (fk_Pokemon_Id_pokemon)
+    REFERENCES Pokemon (Id_pokemon)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE poke_mov ADD CONSTRAINT FK_poke_mov_1
+    FOREIGN KEY (fk_Pokemon_Id_pokemon)
+    REFERENCES Pokemon (Id_pokemon)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE poke_mov ADD CONSTRAINT FK_poke_mov_2
+    FOREIGN KEY (fk_movimentos_Id_Movimento)
+    REFERENCES movimentos (Id_Movimento)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE Poke_elemento ADD CONSTRAINT FK_Poke_elemento_1
+    FOREIGN KEY (fk_Pokemon_Id_pokemon)
+    REFERENCES Pokemon (Id_pokemon)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE Poke_elemento ADD CONSTRAINT FK_Poke_elemento_3
+    FOREIGN KEY (fk_Elemento_id_elemento)
+    REFERENCES Elemento (id_elemento);
+
+USE pokemon;
+
+DELIMITER $$
+
+CREATE TRIGGER criar_party 
+AFTER INSERT ON Treinador 
+FOR EACH ROW
+BEGIN 
+    INSERT INTO party(fk_Treinador_ID_treinador) VALUES(NEW.ID_treinador);
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE TRIGGER libertar_pokemon 
+AFTER DELETE ON Party  -- Adicione o nome da tabela que dispara a trigger
+FOR EACH ROW
+BEGIN
+    UPDATE Pokemon 
+    SET selvagem = true
+    WHERE fk_Party_id_Party = OLD.id_Party;  -- Use o nome correto da coluna
+END$$
+
+DELIMITER ;
+
--- poke_api.py
+++ poke_api.py
@@ -2,6 +2,9 @@ from crud import CRUD
 
 from imports import Flask, CORS
 
+
+
+
 if __name__ == '__main__':
     crud = CRUD()
     crud.open_connection()
